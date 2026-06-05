fe71414e47b07df3c6457dbf54b28bdc9c2f24c6
Anand Rai <93038455+Anandrai08@users.noreply.github.com>
2026-06-05 21:17:50 +0530

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans;
+        for(int i = 0;i<2;i<i++){
+            for(int j=0;j<n;j++){
+                ans.push_back(nums[j]);
+            }
+        }
+        return ans;
+    }
+};

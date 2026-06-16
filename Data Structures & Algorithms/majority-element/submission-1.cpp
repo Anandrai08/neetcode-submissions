e91f674e39158fe2ac9c95ce29ebd7a3593d7d25
Anand Rai <93038455+Anandrai08@users.noreply.github.com>
2026-06-16 07:43:26 +0530

    Add: majority-element - submission-1

---
+++ Data Structures & Algorithms/majority-element/submission-1.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n= nums.size();
+        int count =1;
+        int ans = nums[0];
+
+        for(int i=1;i<n;i++){
+            if(nums[i]==ans) count++;
+            else{
+                count--;
+                if(count<0){
+                    ans = nums[i];
+                    count+=2;
+                }
+            }
+        }
+        return ans;
+    }
+};
